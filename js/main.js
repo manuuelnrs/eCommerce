8d549d657751cee8d4b7fbe0b611767d9473792c
Manuel Nava <juan.nava@fi.unam.edu>
2023-06-17 00:36:31 -0600

    Add audio and video

--- js/main.js
+++ js/main.js
@@ -1,101 +1,12 @@
 // Productos Pre-Cargados
-const productos = [
-    // Pasteles
-    {
-        id: "pastel-01",
-        titulo: "Pastel 01",
-        imagen: "./imagenes/pasteles/01.jpg",
-        categoria: {
-            nombre: "Pasteles",
-            id: "pasteles"
-        },
-        precio: 1000
-    },
-    {
-        id: "pastel-02",
-        titulo: "Pastel 02",
-        imagen: "./imagenes/pasteles/02.jpg",
-        categoria: {
-            nombre: "Pasteles",
-            id: "pasteles"
-        },
-        precio: 1000
-    },
-    {
-        id: "pastel-03",
-        titulo: "Pastel 03",
-        imagen: "./imagenes/pasteles/03.jpg",
-        categoria: {
-            nombre: "Pasteles",
-            id: "pasteles"
-        },
-        precio: 1000
-    },
-    {
-        id: "pastel-04",
-        titulo: "Pastel 04",
-        imagen: "./imagenes/pasteles/04.jpg",
-        categoria: {
-            nombre: "Pasteles",
-            id: "pasteles"
-        },
-        precio: 1000
-    },
-    {
-        id: "pastel-05",
-        titulo: "Pastel 05",
-        imagen: "./imagenes/pasteles/05.jpg",
-        categoria: {
-            nombre: "Pasteles",
-            id: "pasteles"
-        },
-        precio: 1000
-    },
+let productos = [];
 
-    // Crepas
-    {
-        id: "crepa-01",
-        titulo: "Crepa 01",
-        imagen: "./imagenes/crepas/01.jpg",
-        categoria: {
-            nombre: "Crepas",
-            id: "crepas"
-        },
-        precio: 1000
-    },
-    {
-        id: "crepa-02",
-        titulo: "Crepa 02",
-        imagen: "./imagenes/crepas/02.jpg",
-        categoria: {
-            nombre: "Crepas",
-            id: "crepas"
-        },
-        precio: 1000
-    },
-
-    // Bebidas
-    {
-        id: "bebida-01",
-        titulo: "Bebida 01",
-        imagen: "./imagenes/bebidas/01.jpg",
-        categoria: {
-            nombre: "Bebidas",
-            id: "bebidas"
-        },
-        precio: 1000
-    },
-    {
-        id: "bebida-02",
-        titulo: "Bebida 02",
-        imagen: "./imagenes/bebidas/02.jpg",
-        categoria: {
-            nombre: "Bebidas",
-            id: "bebidas"
-        },
-        precio: 1000
-    }
-];
+fetch("./json/productos.json")
+    .then(response => response.json())
+    .then(data => {
+        productos = data;
+        cargarProductos(productos);
+    })
 
 /***        DOM      ***/
 const contenedorProductos = document.querySelector("#contenedor-productos");
