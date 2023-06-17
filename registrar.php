91493a3025a373f8d51bb7a13ab96f978a55c6dd
delux1831 <delux1831@gmail.com>
2023-06-17 13:54:58 -0600

    Update registrar.php

--- registrar.php
+++ registrar.php
@@ -2,17 +2,17 @@
 include("con_db.php");
 
 if (isset($_POST['register'])) {
-    if (strlen($_POST['name']) >= 1 && strlen($_POST['email']) >= 1 && strlen($_POST['edad']) >= 1 && strlen($_POST['telefono']) >= 1 && strlen($_POST['direccion']) >= 1 && strlen($_POST['genero']) >= 1 && strlen($_POST['pais']) >= 1 && strlen($_POST['comentarios']) >= 1){
+    if (strlen($_POST['name']) >= 1 && strlen($_POST['email']) >= 1 && strlen($_POST['edad']) >= 1 && strlen($_POST['telefono']) >= 1 && strlen($_POST['direccion']) >= 1 && strlen($_POST['genero']) >= 1 && strlen($_POST['sucursal']) >= 1 && strlen($_POST['comentarios']) >= 1){
         $name = trim($_POST['name']);
         $edad = trim($_POST['edad']);
         $telefono = trim($_POST['telefono']);
         $direccion = trim($_POST['direccion']);
 		$email = trim($_POST['email']);
         $genero = trim($_POST['genero']);
-        $pais = trim($_POST['pais']); 
+        $sucursal = trim($_POST['sucursal']); 
         $comentarios = trim($_POST['comentarios']);
         $fechareg = date("d/m/y");
-        $consulta = "INSERT INTO datos(nombre, email, edad, telefono, direccion, genero, pais, comentarios, fecha_reg) VALUES ('$name','$email','$edad','$telefono','$direccion','$genero','$pais','$comentarios','$fechareg')"; 
+        $consulta = "INSERT INTO datos(nombre, email, edad, telefono, direccion, genero, sucursal, comentarios, fecha_reg) VALUES ('$name','$email','$edad','$telefono','$direccion','$genero','$pais','$comentarios','$fechareg')"; 
         $resultado = mysqli_query($conex, $consulta);
         if ($resultado) {
             ?>
